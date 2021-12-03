e0e97277e1454904906f034d6b5fee4c3ec8fefa
Wesley <wesleyvitor37417@gmail.com>
2021-12-03 16:27:45 -0300

    Implementação de parte da funcionalidade do botão Salvar No Dispositivo.

--- app/src/main/java/com/example/simpletexteditor/FileUtil.java
+++ app/src/main/java/com/example/simpletexteditor/FileUtil.java
@@ -1,5 +1,9 @@
 package com.example.simpletexteditor;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Environment;
@@ -88,6 +92,81 @@ public class FileUtil {
         }
 
         return rascunho;
+    }
+
+    public static void salvarArquivo(String fileName, String Content, Context c) {
+
+        String raiz = Environment.getExternalStorageDirectory() + File.separator;
+        String diretorio = Environment.DIRECTORY_DOWNLOADS + File.separator;
+        File file = new File(raiz + diretorio + fileName);
+
+        FileOutputStream fluxoDeSaida = null;
+
+        try{
+            if(file.exists()){
+                if(overwriteDialog(c)){
+                    System.out.println("TRUE");
+                    file.delete();
+                }else{
+                    System.out.println("FALSE");
+                    return;
+                }
+            }
+
+            file.createNewFile();
+
+
+        } catch (IOException e) {
+            Log.e(TAG, "Arquivo inacessível", e);
+        }
+
+        try{
+            fluxoDeSaida = new FileOutputStream(file);
+
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Não foi possível acessar o caminho do arquivo.", e);
+        }
+
+        if(fluxoDeSaida!=null){
+            try{
+                fluxoDeSaida.write(Content.getBytes());
+                fluxoDeSaida.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static boolean overwriteDialog(Context c){
+
+        final boolean[] overwrite = {true};
+
+
+        DialogInterface.OnClickListener dialogOnClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                switch (i){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        overwrite[0] = true;
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        overwrite[0] = false;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("");
+        builder.setMessage("O arquivo já existe, deseja sobreescrevê-lo?");
+
+        builder.setPositiveButton("Sim", dialogOnClickListener);
+        builder.setNegativeButton("Não", dialogOnClickListener);
+
+        builder.show();
+
+        return overwrite[0];
     }
 
 }
--- app/src/main/java/com/example/simpletexteditor/MainActivity.java
+++ app/src/main/java/com/example/simpletexteditor/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.simpletexteditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +13,8 @@ import android.widget.EditText;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button carregarRascunhoButton = findViewById(R.id.carregar_rascunho);
         Button salvarRascunhoButton = findViewById(R.id.salvar_rascunho);
-        EditText texto = findViewById(R.id.text_editor);
+        Button salvarArquivoButton = findViewById(R.id.salvar_arquivo);
+        texto = findViewById(R.id.text_editor);
 
         setSupportActionBar(topBar);
         if(getSupportActionBar() != null){
@@ -40,5 +47,37 @@ public class MainActivity extends AppCompatActivity {
                 texto.setText(FileUtil.recuperarRascunho(getAssets()));
             }
         });
+
+        salvarArquivoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fileNameDialog();
+            }
+        });
+    }
+
+    private void fileNameDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Salvar");
+        builder.setMessage("Insira um nome para o arquivo:");
+
+        final EditText fileName = new EditText(this);
+        builder.setView(fileName);
+
+        builder.setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                FileUtil.salvarArquivo(fileName.getText().toString(), texto.getText().toString(), MainActivity.this);
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.show();
     }
 }
